816cf2813e66ccf5cce3430e02f2289afaaa5289
patrikfabijanic <pfabijani@student.unipu.hr>
2024-10-25 20:12:05 +0200

    prvi commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express")
+
+const app = express();
+
+const PORT = 3000;
+
+
+
+
+app.get("/", (req,res) => {
+    res.send("hello world")
+});
+
+
+app.listen(PORT, (error) => {
+    if (error) {
+    console.error(`Greška prilikom pokretanja poslužitelja: ${error.message}`);
+    } else {
+    console.log(`Server je pokrenut na http://localhost:${PORT}`);
+    }
+    });
